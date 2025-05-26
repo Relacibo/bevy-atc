6f1ff8300292e799d7a4c4a4bcd750855fa98748
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-05-26 20:33:38 +0200

    weird math

--- src/game/heading.rs
+++ src/game/heading.rs
@@ -7,7 +7,7 @@ pub struct Heading(f64);
 
 impl From<f64> for Heading {
     fn from(value: f64) -> Self {
-        Heading(value % 360.)
+        Heading(value.rem_euclid(360.))
     }
 }
 
@@ -23,14 +23,14 @@ impl Sub for Heading {
     type Output = f64;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        (self.0 - rhs.0) % 360.
+        (self.0 - rhs.0).rem_euclid(360.)
     }
 }
 
 impl Heading {
     pub fn change(self, heading_change: f64) -> Heading {
         let Heading(heading) = self;
-        let res = (heading + heading_change) % 360.;
+        let res = (heading + heading_change).rem_euclid(360.);
         Heading(res)
     }
 
@@ -47,9 +47,9 @@ impl Heading {
     pub fn required_change(self, cleared: Heading) -> f64 {
         let distance = self - cleared;
         if distance < 180.0 {
-            distance
+            -distance
         } else {
-            -360. + distance
+            360. - distance
         }
     }
 
